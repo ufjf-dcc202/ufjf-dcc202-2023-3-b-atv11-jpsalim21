bce35d084cff89805a8d62daa83459394e4509cf
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2023-12-03 18:52:46 -0300

    Variáveis para receber os elementos html

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ import {
     limpaLista
 } from './lista.js';
 
+const entrada = document.querySelector('#entrada');
+const botaoAdicionar = document.querySelector('#adicionar');
+const botaoLimpar = document.querySelector('#limpar');
+const itens = document.querySelector('#itens');
+
